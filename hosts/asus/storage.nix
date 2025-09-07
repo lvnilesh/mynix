6ac28e7a128f4031367c1c70f876c2adcdec631d
Nilesh <nilesh@cloudgeni.us>
2025-09-06 20:39:31 -0700

    storage devices updated

--- hosts/asus/storage.nix
+++ hosts/asus/storage.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{...}: {
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/4f47c9a3-f61d-4cee-8f64-d38088a7056c";
     fsType = "ext4";
@@ -7,10 +7,10 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/A315-BF00";
     fsType = "vfat";
-    options = [ "fmask=0077" "dmask=0077" ];
+    options = ["fmask=0077" "dmask=0077"];
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/470abbe0-ef13-45a7-aa84-f15ef9ad755d"; }
+    {device = "/dev/disk/by-uuid/470abbe0-ef13-45a7-aa84-f15ef9ad755d";}
   ];
 }
