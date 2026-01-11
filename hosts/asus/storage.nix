c85c2d50f4b65a3043385000a8771e02a480b382
Nilesh <nilesh@cloudgeni.us>
2026-01-10 22:49:41 -0800

    dont mount windows SSD

--- hosts/asus/storage.nix
+++ hosts/asus/storage.nix
@@ -11,11 +11,12 @@
   };
 
   # NVMe data drives
-  fileSystems."/mnt/nvme-2tb" = {
-    device = "/dev/disk/by-uuid/d191e724-2ab0-4fd5-9c6b-3f732d9b0a18";
-    fsType = "ext4";
-    options = ["defaults" "nofail"];
-  };
+  # nvme-2tb now has Windows installed - commented out
+  # fileSystems."/mnt/nvme-2tb" = {
+  #   device = "/dev/disk/by-uuid/d191e724-2ab0-4fd5-9c6b-3f732d9b0a18";
+  #   fsType = "ext4";
+  #   options = ["defaults" "nofail"];
+  # };
 
   fileSystems."/mnt/nvme-1tb" = {
     device = "/dev/disk/by-uuid/ecf00eae-cf47-40c8-9bea-b47dd32d98ee";
