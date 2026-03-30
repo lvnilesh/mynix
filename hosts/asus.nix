68e66b284d4b055d3aad3ededb4ea5f49a35471b
Nilesh <nilesh@cloudgeni.us>
2026-03-30 04:58:51 -0700

    keyboard shortcuts for hypr

--- hosts/asus.nix
+++ hosts/asus.nix
@@ -46,6 +46,7 @@
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", KERNEL=="0000:3d:00.0", ATTR{power/control}="on"
   '';
 
+  programs.gpu-screen-recorder.enable = true;
   services.printing.enable = true;
   services.tailscale.enable = true;
   services.twitter-chatbot.enable = true;
